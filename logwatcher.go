ece3b8c37e5764d855b510629e87763aff9e2c68
aklan <aplanas9009@gmail.com>
2020-11-01 18:04:52 +0200

    Add better error handling

--- logwatcher.go
+++ logwatcher.go
@@ -3,13 +3,13 @@ package main
 import (
 	"encoding/base64"
 	"io/ioutil"
-	"log"
 	"net/smtp"
 	"os"
 	"regexp"
 	"strings"
 
 	"github.com/hpcloud/tail"
+	logger "github.com/sirupsen/logrus"
 )
 
 // Smtp server constants
@@ -17,50 +17,48 @@ const smtpServer string = "" // smtp server addr
 const smtpPort string = "25"
 const servername = "/etc/hostname"
 
-func checkError(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func getHostname() string {
-	hostname, err := ioutil.ReadFile(servername)
-	checkError(err)
-	return string(hostname)
+	if hostname, err := ioutil.ReadFile(servername); err == nil {
+		return string(hostname)
+	}
+	return ""
 }
 
 // Send email notification
-func emailNotify(str string) {
+func emailNotify(str string) error {
 	replacer := strings.NewReplacer("\r\n", "", "\r", "", "\n", "", "%0a", "", "%0d", "")
 	msgBody := []byte(str) // Message of notification
 
 	// email details
 	sender := "no-reply@bosch.io"
-	toList := strings.Split("", ",") // comma separated string with receivers email addr
+	to := strings.Split("", ",") // comma separated string with receivers email addr
 	hostname := replacer.Replace(getHostname())
-	subject := " " + hostname //
+	subject := "Errors in server " + hostname //
 
 	// Dial smtp server for retrieve a connection
 	conn, dialErr := smtp.Dial(smtpServer + ":" + smtpPort)
-	checkError(dialErr)
+	if dialErr != nil {
+		return dialErr
+	}
 	defer conn.Close()
 
 	// Set sender
-	senderErr := conn.Mail(replacer.Replace(sender))
-	checkError(senderErr)
+	conn.Mail(replacer.Replace(sender))
 
 	// Build receipient list
-	for i := range toList {
-		toList[i] = replacer.Replace(toList[i])
-		recpErr := conn.Rcpt(toList[i])
-		checkError(recpErr)
+	for i := range to {
+		if recpErr := conn.Rcpt(replacer.Replace(to[i])); recpErr != nil {
+			logger.Info("Error setting receipient " + to[i])
+		}
 	}
 
-	wr, bodyErr := conn.Data()
-	checkError(bodyErr)
+	wr, errData := conn.Data()
+	if errData != nil {
+		return errData
+	}
 
 	// Message structure
-	msg := "To: " + strings.Join(toList, ",") + "\r\n" +
+	msg := "To: " + strings.Join(to, ",") + "\r\n" +
 		"From: " + sender + "\r\n" +
 		"Subject: " + subject + "\r\n" +
 		"Content-Type: text/html; charset=\"UTF-8\"\r\n" +
@@ -68,13 +66,15 @@ func emailNotify(str string) {
 		"\r\n" + base64.StdEncoding.EncodeToString(msgBody)
 
 	// Write message into email body
-	_, writeErr := wr.Write([]byte(msg))
-	checkError(writeErr)
+	wr.Write([]byte(msg))
 
 	// Close connection
-	closeWErr := wr.Close()
-	checkError(closeWErr)
+	if err := wr.Close(); err != nil {
+		return err
+	}
+
 	conn.Quit()
+	return nil
 }
 
 // Watch logs files for errors
@@ -83,12 +83,18 @@ func watchLogs(path string) {
 
 	// Read from tail of file
 	t, err := tail.TailFile(path, tail.Config{Follow: true, ReOpen: true})
+	if err != nil {
+		logger.Error(err)
+	}
 
-	checkError(err)
 	// if any line is error send a email notification
 	for line := range t.Lines {
 		if errorPattern.MatchString(line.Text) {
-			emailNotify(line.Text)
+			if er := emailNotify(line.Text); er != nil {
+				logger.Info("Email notification was not sent. Cause: ", er)
+			} else {
+				logger.Info("Notification sent to recipient list!")
+			}
 		}
 	}
 }
